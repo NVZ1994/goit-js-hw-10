8471c3d891992ec4a8e9608267a5bf790be8aee0
NVZ1994 <110624272+NVZ1994@users.noreply.github.com>
2023-02-09 14:34:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+// import artTempl from './template/articles.hbs';
+import fetchCountries from './fetch.js'
+import { Notify } from 'notiflix/build/notiflix-notify-aio'
+const DEBOUNCE_DELAY = 500;
+const START_URL = "https://restcountries.com/v2/name/";
+const inputEl = document.getElementById("search-box");
+const listEl = document.querySelector("ul");
+const containerEl = document.querySelector(".country-info");
+const MAX_COUNTRIES = 10;
+let input = "";
+inputEl.addEventListener("input", debounce((ev) => {
+    input = ev.target.value.trim(" ");
+    listEl.innerHTML = '';
+    containerEl.innerHTML = "";
+    if (!input) {
+        return;
+    }
+    fetchCountries(START_URL, input).then(data => {
+        if (data.length > MAX_COUNTRIES) {
+            return Notify.info("Too many matches found. Please enter a more specific name.");
+        }
+        else if (data.length > 1) {
+            listEl.insertAdjacentHTML("beforeend", markupCountries(data))
+        }
+        else if (data.length === 1) {
+            containerEl.insertAdjacentHTML("beforeend", markupCountrie(data))
+        }
+    }).catch();
+}, DEBOUNCE_DELAY));
+function addArticles(data) {
+}
 
-const DEBOUNCE_DELAY = 300;
+function markupCountries(items) {
+    return items.map(item => `<li><img src="${item.flag}" alt="flag" width="40" height ="30">
+            <p>  ${item.name}</p></li>`).join("");
+}
+function markupCountrie(item) {
+    return item.map(country => `<div class ="wraper">
+    <img src="${country.flag}" alt="flag" width="70" height ="40">
+            <h2>  ${country.name}</h2></div><p><b>Capital: </b>${country.capital}</p>
+            <p><b>Population: </b>${country.population}</p>
+            <p><b>Languages: </b>${country.languages.map(lang => lang.name).join(", ")}</p>`).join("");
+}
